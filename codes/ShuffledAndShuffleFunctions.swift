1e10b8c7cbb08fa191bcb4084c9f39f44b4473bf
Emre Özdil <emreozdil@icloud.com>
2018-11-18 15:36:55 +0300

    Add shuffled and shuffle functions

---
+++ codes/ShuffledAndShuffleFunctions.swift
@@ -0,0 +1,9 @@
+// Shuffled and shuffle functions
+
+var numbers = [1, 2, 3, 4, 5, 6]
+let shuffledNumbers = numbers.shuffled()
+// shuffledNumbers == [6, 1, 3, 2, 5, 4]
+// numbers == [1, 2, 3, 4, 5, 6]
+
+numbers.shuffle()
+// numbers == [4, 2, 6, 5, 1, 3]
