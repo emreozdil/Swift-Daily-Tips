3d045ed027a220f758cc3ba7f8efbd754fa7393b
Christoph Aigner <aignerc93@users.noreply.github.com>
2018-11-25 21:44:39 +0100

    Update StaticTypeProperties.swift
    Updated description

--- codes/StaticTypeProperties.swift
+++ codes/StaticTypeProperties.swift
@@ -1,9 +1,9 @@
 // Static - Type Properties
 
-// The main benefit here is, that you no longer have to create an instance to access properties.
 // This 'Constant' enum has static type properties.
-// You could also use struct instead of the enum, but this allows for the creation of instances (unless the init is private).
-// By using the enum without a case you prevent the unintentional creation of instances.
+// The main benefit is, that you no longer have to create an instance to access properties.
+// You could also use a struct instead of the enum, but this allows for the creation of instances (unless the init is private).
+// By using an enum without a case you prevent the unintentional creation of instances.
 
 enum Constant {
     static let baseUrl = "https://github.com/MobileTipsters/Swift-Daily-Tips/"
