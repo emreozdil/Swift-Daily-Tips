1ceddd932e5055aac3aefe54a8702af41748ad79
Emre Özdil <emreozdil@icloud.com>
2018-12-04 17:01:17 +0300

    Change example code line

--- codes/Builder.swift
+++ codes/Builder.swift
@@ -1,11 +1,4 @@
-/// Builder allows you to create an instance and setting it up in a simple way:
-/// ✅ Example: ✅
-//	private let tableView = UITableView(frame: .zero, style: .plain).with { tableView in
-//		tableView.backgroundColor = .white
-//		tableView.separatorColor = .darkGray
-//		tableView.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 10.0, right: 0)
-//		tableView.allowsMultipleSelection = true
-//	}
+// Builder allows you to create an instance and setting it up in a simple way:
 
 protocol Builder {}
 
@@ -19,3 +12,10 @@ extension Builder {
 
 // We need NSObject to conform to Builder to enable it for all subclasses of it 😊
 extension NSObject: Builder {}
+
+private let tableView = UITableView(frame: .zero, style: .plain).with { tableView in
+    tableView.backgroundColor = .white
+    tableView.separatorColor = .darkGray
+    tableView.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 10.0, right: 0)
+    tableView.allowsMultipleSelection = true
+}
