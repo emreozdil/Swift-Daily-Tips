7b265074fffbd6881b7b2d5819e7d5f427619a04
Emre Özdil <emreozdil@icloud.com>
2018-12-05 17:39:30 +0300

    Add Multiline String Literals

---
+++ codes/MultilineStringLiterals.swift
@@ -0,0 +1,12 @@
+// Multiline String Literals
+// If you need a string that spans several lines, use
+// a multiline string literal—a sequence of characters
+// surrounded by three double quotation marks:
+
+let quotation = """
+The White Rabbit put on his spectacles.  "Where shall I begin,
+please your Majesty?" he asked.
+
+"Begin at the beginning," the King said gravely, "and go on
+till you come to the end; then stop."
+"""
