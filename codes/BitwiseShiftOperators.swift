85e20922714ac1898ffdd4f1a83b9f4407b6fcc5
Emre Özdil <emreozdil@icloud.com>
2018-12-17 19:22:59 +0300

    Fix code snippet comment

--- codes/BitwiseShiftOperators.swift
+++ codes/BitwiseShiftOperators.swift
@@ -1,8 +1,12 @@
 // Bitwise Left and Right Shift Operators
-// The bitwise left shift operator (<<) and bitwise right
-// shift operator (>>) move all bits in a number to the
-// left or the right by a certain number of places,
-// according to the rules defined below.
+// The bitwise left shift operator (<<) and bitwise right shift operator (>>)
+// move all bits in a number to the left or the right by a certain number of
+// places, according to the rules defined below.
+
+// Bitwise left and right shifts have the effect of multiplying or dividing
+// an integer by a factor of two. Shifting an integer’s bits to the left by
+// one position doubles its value, whereas shifting it to the right by one
+// position halves its value.
 
 let number = 1024
 let divisionByTwo = number >> 1
