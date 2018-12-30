e186028cc0f9b94d4c0478d80036383ffeefe188
Emre Özdil <emreozdil@icloud.com>
2018-12-30 23:17:29 +0300

    Add Sketch Shadow Extension

---
+++ codes/SketchShadow.swift
@@ -0,0 +1,27 @@
+// Sketch Shadow
+
+extension CALayer {
+    func applySketchShadow(
+        color: UIColor = .black,
+        alpha: Float = 0.5,
+        x: CGFloat = 0,
+        y: CGFloat = 2,
+        blur: CGFloat = 4,
+        spread: CGFloat = 0)
+    {
+        shadowColor = color.cgColor
+        shadowOpacity = alpha
+        shadowOffset = CGSize(width: x, height: y)
+        shadowRadius = blur / 2.0
+        if spread == 0 {
+            shadowPath = nil
+        } else {
+            let dx = -spread
+            let rect = bounds.insetBy(dx: dx, dy: dx)
+            shadowPath = UIBezierPath(rect: rect).cgPath
+        }
+    }
+}
+
+layer.applySketchShadow()
+layer.applySketchShadow(color: .red, alpha: 0.6, x: 0, y: 4, blur: 8, spread: -4)
