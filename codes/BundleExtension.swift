b8789acb82629fedf0d5b43344bdedd54f9f70d0
Emre Özdil <emreozdil@icloud.com>
2019-01-19 23:46:32 +0300

    Add Bundle Extension

---
+++ codes/BundleExtension.swift
@@ -0,0 +1,17 @@
+extension Bundle {
+    var appName: String {
+        return infoDictionary?["CFBundleName"] as! String
+    }
+    
+    var bundleId: String {
+        return bundleIdentifier!
+    }
+    
+    var versionNumber: String {
+        return infoDictionary?["CFBundleShortVersionString"] as! String
+    }
+    
+    var buildNumber: String {
+        return infoDictionary?["CFBundleVersion"] as! String
+    }
+}
