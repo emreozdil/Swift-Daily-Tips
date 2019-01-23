74a1b68b5f96594ef698521d37f339093656aeef
Emre Özdil <emreozdil@icloud.com>
2019-01-23 23:49:35 +0300

    Add Decoding JSON from Bundle

---
+++ codes/DecodingJSONFromBundle.swift
@@ -0,0 +1,23 @@
+// Decoding JSON from bundle
+
+extension Bundle {
+    func decode<T: Decodable>(_ type: T.Type, from filename: String) -> T {
+        guard let json = url(forResource: filename, withExtension: nil) else {
+            fatalError("Failed to locate \(filename) in app bundle.")
+        }
+        
+        guard let jsonData = try? Data(contentsOf: json) else {
+            fatalError("Failed to load \(filename) from app bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let result = try? decoder.decode(T.self, from: jsonData) else {
+            fatalError("Failed to decode \(filename) from app bundle.")
+        }
+        
+        return result
+    }
+}
+
+let items = Bundle.main.decode([ResponseItem].self, from: "Response.json")
