913cd8ed177692e5340d76012e6a5e7462a444cd
Emre Özdil <emreozdil@icloud.com>
2019-01-25 23:49:44 +0300

    Add Url Query Parameters

---
+++ codes/UrlQueryParameters.swift
@@ -0,0 +1,16 @@
+// Dictionary of the URL's query parameters
+
+extension URL {
+    var queryParameters: [String: String]? {
+        guard let components = URLComponents(url: self, resolvingAgainstBaseURL: false) else { return nil }
+        guard let queryItems = components.queryItems else { return nil }
+
+        var items: [String: String] = [:]
+
+        for queryItem in queryItems {
+            items[queryItem.name] = queryItem.value
+        }
+
+        return items
+    }
+}
