ae2bff2490171f998b03d7c24742a9ea92afa4c8
Emre Özdil <emreozdil@icloud.com>
2019-01-26 22:30:51 +0300

    Add Int to UInt, Double, Float

---
+++ codes/IntToUIntDoubleFloat.swift
@@ -0,0 +1,15 @@
+// Int to UInt, Double, Float
+
+extension Int {
+    var uInt: UInt {
+        return UInt(self)
+    }
+
+    var double: Double {
+        return Double(self)
+    }
+
+    var float: Float {
+        return Float(self)
+    }
+}
