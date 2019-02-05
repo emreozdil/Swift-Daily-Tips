2278ca45dde5151ee5df0e2df75b9e1a36eff9a6
Emre Özdil <emreozdil@icloud.com>
2019-02-05 22:59:20 +0300

    Add Custom Operator

---
+++ codes/CustomOperator.swift
@@ -0,0 +1,13 @@
+// Custom Operator
+
+extension Vector2D {
+    static prefix func +++ (vector: inout Vector2D) -> Vector2D {
+        vector += vector
+        return vector
+    }
+}
+
+var toBeDoubled = Vector2D(x: 1.0, y: 4.0)
+let afterDoubling = +++toBeDoubled
+// toBeDoubled now has values of (2.0, 8.0)
+// afterDoubling also has values of (2.0, 8.0)
